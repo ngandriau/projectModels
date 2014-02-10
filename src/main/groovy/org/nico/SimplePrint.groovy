7d83de1a2f5c71a805041f1e3510a98a388d51c5
Nicolas Gandriau | hybris <nicolas.gandriau@hybris.com>
2014-02-10 11:23:34 -0500

    started work on feature f1 - simple print

---
+++ src/main/groovy/org/nico/SimplePrint.groovy
@@ -0,0 +1,11 @@
+package org.nico
+
+/**
+ * Created by User: ngandriau - Date: 2/10/14 - Time: 11:22 AM
+ */
+class SimplePrint {
+
+    public static void main(String[] args) {
+        println "hello simple print"
+    }
+}
