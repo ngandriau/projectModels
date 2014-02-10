4c7d876452403d99e859f11fb84e60fc13bfb40d
Nicolas Gandriau | hybris <nicolas.gandriau@hybris.com>
2014-02-10 11:27:35 -0500

    started work on feature f2 - better print

---
+++ src/main/groovy/org/nico/BetterPrint.groovy
@@ -0,0 +1,11 @@
+package org.nico
+
+/**
+ * Created by User: ngandriau - Date: 2/10/14 - Time: 11:26 AM
+ */
+class BetterPrint {
+
+    public static void main(String[] args) {
+        println "better print!!!"
+    }
+}
