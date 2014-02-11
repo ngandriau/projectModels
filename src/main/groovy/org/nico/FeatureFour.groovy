0304e5addc317bc23677c9a78d7a461ef68503d2
Nicolas Gandriau | hybris <nicolas.gandriau@hybris.com>
2014-02-11 14:41:01 -0500

    end of working on feature4

--- src/main/groovy/org/nico/FeatureFour.groovy
+++ src/main/groovy/org/nico/FeatureFour.groovy
@@ -4,4 +4,8 @@ package org.nico
  * Created by User: ngandriau - Date: 2/11/14 - Time: 2:36 PM
  */
 class FeatureFour {
+
+    public static void main(String[] args) {
+        println "done"
+    }
 }
