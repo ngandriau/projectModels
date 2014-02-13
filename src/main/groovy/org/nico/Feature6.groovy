38aab51eb2725768ed41b58415c0687190c32ac6
Nicolas Gandriau | hybris <nicolas.gandriau@hybris.com>
2014-02-13 11:57:35 -0500

    f6 progress

--- src/main/groovy/org/nico/Feature6.groovy
+++ src/main/groovy/org/nico/Feature6.groovy
@@ -4,4 +4,8 @@ package org.nico
  * Created by User: ngandriau - Date: 2/13/14 - Time: 11:52 AM
  */
 class Feature6 {
+
+    public static void main(String[] args) {
+        println "now f6 does something"
+    }
 }
